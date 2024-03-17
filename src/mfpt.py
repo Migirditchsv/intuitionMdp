fixed mfpt calculation. Bug in mdp policy value iteration solver

src/mfpt.py
@@ -6,69 +6,65 @@ from src.visualization import plot_transition_matrix
 
 
 def construct_transition_matrix(policy_grid, action_space, stochasticity):
-    # The following code is based on (Debnath 2019 paper, eq 8). It constructs the transition matrix for the given policy
     N = len(policy_grid)  # Size of the grid
-    M= N*N
+    M = N * N
     transition_matrix = np.zeros((M, M))
 
     for i in range(N):
         for j in range(N):
             policy_action = policy_grid[i][j]
             for action in action_space.values():
-                #if np.array_equal(policy_action, action):
-                    new_state = add_tuples((i, j), action)
-                    # Ensure the new state is within bounds
-                    if 0 <= new_state[0] < N and 0 <= new_state[1] < N:
-                        # Convert 2D indices to 1D index
-                        old_state_1d = i * N + j
-                        new_state_1d = new_state[0] * N + new_state[1]
+                new_state = add_tuples((i, j), action)
+                # Ensure the new state is within bounds
+                if 0 <= new_state[0] < N and 0 <= new_state[1] < N:
+                    # Convert 2D indices to 1D index
+                    old_state_1d = i * N + j
+                    new_state_1d = new_state[0] * N + new_state[1]
 
-                        # Update the transition matrix: If the action is stationary and NOT stochastic, the value is p_ii - 1 =  1 - stochasticity -1 = stochasticity
-                        if old_state_1d == new_state_1d and np.array_equal(action, policy_action):
-                            transition_matrix[old_state_1d][new_state_1d] += stochasticity
-
-                        # If the policy action is stationary, and the action IS stoch, the value is stochasticity - 1
-                        elif old_state_1d == new_state_1d and not np.array_equal(action, policy_action):
-                            transition_matrix[old_state_1d][new_state_1d] += stochasticity -1
-                        # If the policy isn't stationary, and the action is the policy action, the value is p = 1 - stochasticity
-                        elif old_state_1d != new_state_1d and np.array_equal(action, policy_action):
-                            transition_matrix[old_state_1d][new_state_1d] += 1 - stochasticity
-
-                        elif old_state_1d != new_state_1d and not np.array_equal(action, policy_action):
-                            transition_matrix[old_state_1d][new_state_1d] += stochasticity
+                    # Update the transition matrix based on Debnath et al. (2019) eq (8)
+                    if old_state_1d == new_state_1d: # If T_ii
+                        if np.array_equal(action, policy_action):
+                            transition_matrix[old_state_1d][new_state_1d] += -stochasticity
                         else:
-                            raise IndexError("ERROR: Weird transition action taken from state" + str((i, j)) + " to state " +
-                                  str(new_state) + " with action " + str(action) + " and policy action " +
-                                  str(policy_action))
-                            exit(1)
-
+                            transition_matrix[old_state_1d][new_state_1d] += stochasticity
+                    else:
+                        if np.array_equal(action, policy_action):
+                            transition_matrix[old_state_1d][new_state_1d] += 1 - stochasticity
+                        else:
+                            transition_matrix[old_state_1d][new_state_1d] += stochasticity
 
     # Subtract diagonal elements from 1
-    for i in range(N*N):
+    for i in range(M):
         transition_matrix[i][i] = 1 - transition_matrix[i][i]
 
     return transition_matrix
+
 
 def compute_mfpt(policy_grid, action_space, stochasticity):
 
     # Construct the transition matrix
     transition_matrix = construct_transition_matrix(policy_grid, action_space, stochasticity)
-    #plot_transition_matrix(transition_matrix)
+
     if is_singular(transition_matrix):
         print("WARNING: The transition matrix is singular, and the mean first passage time cannot be computed."
               "\n Adding a small constant to the diagonal elements to remove singularity.")
         transition_matrix = remove_singularity(transition_matrix)
     # Convert T to a sparse matrix in CSR format
-    T_sparse = csr_matrix(transition_matrix)
+    #T_sparse = csr_matrix(transition_matrix)
+
+    # Simply invert Trans matrix
+    T_inv = np.linalg.inv(transition_matrix)
 
     # Create a negative vector of ones
     id_vector = -np.ones(transition_matrix.shape[0])
 
     # Solve the system
-    mu = spsolve(T_sparse, id_vector)
+    mu = T_inv @ id_vector
 
-    # Reshape mu into an NxN matrix
     N = int(np.sqrt(len(mu)))
+    # Assert that N is the same size as the policy grid
+    assert N == len(policy_grid)
+    # Reshape mu into an NxN matrix
     mu_matrix = np.zeros((N, N))
     for i in range(N):
         for j in range(N):

src/policy_value_iteration.py
@@ -4,7 +4,7 @@ import numpy as np
 def policy_value_iteration(initialCondition, action_space, stochasticity, movement_cost_scale):
     N = len(initialCondition)  # Size of the grid
     value_grid = np.copy(initialCondition)  # Copy the initial conditions to the value grid
-    movement_cost = movement_cost_scale / (2 * N)  # Movement cost for non-stationary actions
+    movement_cost = - movement_cost_scale / (N * N)  # Movement cost for non-stationary actions
 
     # Initialize grids for values and policies, and max_delta_value
     new_value_grid = np.zeros((N, N))

src/visualization.py
@@ -1,6 +1,6 @@
 import numpy as np
 import seaborn as sns
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, colors
 from matplotlib.patches import FancyArrowPatch
 
 
@@ -52,9 +52,27 @@ def plot_transition_matrix(transition_matrix):
     plt.ylabel('State i')
     plt.show()
 
+
 def plot_mu_matrix(mu_matrix):
     plt.figure(figsize=(10, 8))
-    sns.heatmap(mu_matrix, cmap='rocket', annot=True, fmt=".2f")
+
+    # Create a colormap for the heatmap
+    cmap = plt.get_cmap('rocket')
+    norm = colors.Normalize(vmin=mu_matrix.min(), vmax=mu_matrix.max())
+
+    # Create the heatmap using imshow
+    plt.imshow(mu_matrix, cmap=cmap, norm=norm)
+
+    # Add a colorbar
+    plt.colorbar(label='Value')
+
+    # Add annotations
+    # for i in range(mu_matrix.shape[0]):
+    #     for j in range(mu_matrix.shape[1]):
+    #         text = plt.text(j, i, np.round(mu_matrix[i, j], 2),
+    #                        ha="center", va="center", color="w")
+
+
     plt.title('Mu Matrix Heatmap')
     plt.xlabel('State j')
     plt.ylabel('State i')